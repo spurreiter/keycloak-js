70a89a23afa78cd48041bc7a90c3f887e5824659
spurreiter <spurreiter@users.noreply.github.com>
2020-07-04 12:10:30 +0200

    chore: use fetch instead of module update task

--- scripts/copy.js
+++ scripts/copy.js
@@ -9,23 +9,35 @@ const srcDir = `${__dirname}/../node_modules/keycloak-js`
 
 const tasks = {}
 
-tasks.module = async () => {
+tasks.checkout = async () => {
   cd(dir)
   exec('git checkout -b keycloak')
-  rm('-rf', 'node_modules/keycloak-js')
+}
+
+tasks.module = async () => {
+  await tasks.checkout()
+  cd(dir)
   exec('npm i')
   cp(`${srcDir}/dist/keycloak.js`, `${dir}`)
+  await tasks.lint()
 }
 
 tasks.fetch = async () => {
+  await tasks.checkout()
+  cd(dir)
   const url = 'https://raw.githubusercontent.com/keycloak/keycloak/master/adapters/oidc/js/src/main/resources/keycloak.js'
   await fetch(url).then(res => {
     if (!res.ok) throw new Error(`unexpected response ${res.statusText}`)
     return streamPipeline(res.body, fs.createWriteStream(`${dir}/keycloak.js`))
   })
+  await tasks.lint()
+}
+
+tasks.lint = async () => {
+  exec('npm run lint')
 }
 
 ;(async () => {
-  const task = process.argv.slice(2)[0] || 'module'
+  const task = process.argv.slice(2)[0] || 'fetch'
   if (tasks[task]) await tasks[task]()
 })()
